db2753240588ea1d227bffb7f1c9cc8b8aad043d
TengMengHouy <la9089975@gmail.com>
2026-05-04 11:23:28 +0700

    fix: remove @EntityScan (removed in Spring Boot 4.0), use JpaConfig

--- tunnel-server/src/main/java/app/stadoor/tunnel/TunnelServerApplication.java
+++ tunnel-server/src/main/java/app/stadoor/tunnel/TunnelServerApplication.java
@@ -2,12 +2,8 @@ package app.stadoor.tunnel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-@EntityScan(basePackages = "app.stadoor.common.entity")
-@EnableJpaRepositories(basePackages = "app.stadoor.common.repository")
+@SpringBootApplication(scanBasePackages = "app.stadoor")
 public class TunnelServerApplication {
 
     public static void main(String[] args) {
